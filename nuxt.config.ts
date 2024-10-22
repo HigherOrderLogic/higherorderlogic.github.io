d4585ad7ca1f0c67824b52f23fbd804727eefc04
Horu <73709188+HigherOrderLogic@users.noreply.github.com>
2024-10-22 15:04:27 +1100

    feat: improve SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+const NAME = 'Horu'
+const DESCRIPTION = 'Horu\'s personal website'
+
 export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
@@ -13,8 +16,18 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'Horu.me',
+      title: NAME,
       htmlAttrs: { lang: 'en' },
+      meta: [
+        { name: 'apple-mobile-web-app-title', content: NAME },
+        { name: 'application-name', content: NAME },
+        { name: 'twitter:title', content: NAME },
+        { name: 'twitter:description', content: DESCRIPTION },
+        { name: 'twitter:card', content: 'summary' },
+        { property: 'og:title', content: NAME },
+        { property: 'og:description', content: DESCRIPTION },
+        { property: 'og:type', content: 'website' },
+      ],
     },
   },
 
